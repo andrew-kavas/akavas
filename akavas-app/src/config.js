ccfa1fd1cc6ac6ef808b120c66bc0ae888d8ee00
Andrew Kavas <askavas410@gmail.com>
2025-11-06 00:55:13 -0600

    fix prod env config

--- akavas-app/src/config.js
+++ akavas-app/src/config.js
@@ -1,26 +1,24 @@
-const { process } = globalThis;
+// Safely access environment variables defined in vite.config.js
+const VERSION = process.env.VERSION || 'development';
+const AKAVAS_API_URL = process.env.AKAVAS_API_URL;
+const AKAVAS_APP_URL = process.env.AKAVAS_APP_URL;
 
-let { env } = process;
+// Set defaults for development
+let apiUrl = AKAVAS_API_URL;
+let appUrl = AKAVAS_APP_URL;
 
-const { VERSION: version } = env;
-
-if (version === 'development') {
-  env = {
-    ...env,
-    AKAVAS_API_URL: 'http://localhost:4000',
-    AKAVAS_APP_URL: 'http://localhost:3000'
-  };
+if (VERSION === 'development') {
+  apiUrl = apiUrl || 'http://localhost:4000';
+  appUrl = appUrl || 'http://localhost:3000';
 }
 
 const config = {
   akavas: {
-    // apiUrl: 'https://akavas-api.fly.dev',
-    apiUrl: env.AKAVAS_API_URL,
-    // appUrl: 'https://akavas-app.fly.dev'
-    appUrl: env.AKAVAS_APP_URL
+    apiUrl,
+    appUrl
   },
-  port: version === 'development' ? 3000 : 80,
-  version
+  port: VERSION === 'development' ? 3000 : 80,
+  version: VERSION
 };
 
 export default config;
